23912447299a604b0b79547ead209e1489f9c82e
VanskyX-ctrl <rianjung46@gmail.com>
2026-04-07 15:00:20 +0700

    calculator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+Console.WriteLine("==================");
+Console.WriteLine("Pilih Angka 1-4: ");
+Console.WriteLine("------------------");
+Console.WriteLine("1. Penjumlahan");
+Console.WriteLine("2. Pengurangan");
+Console.WriteLine("3. Perkalian");
+Console.WriteLine("4. Pembagian");
+Console.WriteLine("==================");
+int p = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Masukan Angka Pertama: ");
+int var1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Masukan Ankga Kedua: ");
+int var2 = int.Parse(Console.ReadLine()!);
+if (p == 1)
+{
+    Console.WriteLine("Hasil: " + (var1 + var2));
+}
+else if (p == 2)
+{
+    Console.WriteLine("Hasil: " + (var1 - var2));
+}
+else if (p == 3)
+{
+    Console.WriteLine("Hasil: " + (var1 * var2));
+}
+else if (p == 4)
+{
+    Console.WriteLine("Hasil: " + (var1 / var2));
+}
